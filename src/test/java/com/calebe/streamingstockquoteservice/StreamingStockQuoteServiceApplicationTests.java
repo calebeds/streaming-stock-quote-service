26c811e55fa386134ea2529a22a856abbb790282
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-26 21:03:15 -0300

    initial commit

---
+++ src/test/java/com/calebe/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calebe.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
